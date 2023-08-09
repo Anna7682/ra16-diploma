22849054166e45270d773954b578e4c1a0e4bbfb
Anna7682 <107514746+Anna7682@users.noreply.github.com>
2023-08-09 19:56:41 +0300

    Delete Skeleton.tsx

--- src/components/Skeleton/Skeleton.tsx
+++
@@ -1,8 +0,0 @@
-import React, { ReactElement } from 'react';
-import './Skeleton.css';
-
-export type Props = React.HTMLAttributes<HTMLDivElement>;
-
-export default function Skeleton({ className, ...props }: Props): ReactElement {
-  return <div {...props} className="skeleton" />;
-}
