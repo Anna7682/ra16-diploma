4bdf7bae2038a07e73c9a343cf2ac98ab2871b04
Anna7682 <107514746+Anna7682@users.noreply.github.com>
2023-08-09 19:26:28 +0300

    Update Error.tsx

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,30 +1,29 @@
-import React, { ReactElement } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Alert from '@material-ui/lab/Alert';
+import './Error.scss';
+import { SerializedError } from '@reduxjs/toolkit';
+import { useAppDispatch } from '../../hooks/hooks';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-    '& > * + *': {
-      marginTop: theme.spacing(2),
-    },
-  },
-}));
+type TPropsError = {
+  error: SerializedError,
+  text: string,
+  clossest: string,
+  callback: Function
+}
 
-export type Props = {
-  message: string;
-  className?: string;
-  onTransitionEnd?: () => void;
-};
+export function Error({ error, text, clossest, callback }: TPropsError): JSX.Element {
+  const dispatch = useAppDispatch();
 
-export default function Error({ message, className }: Props): ReactElement {
-  const classes = useStyles();
+  const handleClick = (e: React.MouseEvent) => {
+    if (error && e.currentTarget.closest(clossest)) {
+      dispatch(callback())
+    }
+  }
 
   return (
-    <div className={`${classes.root} shadow ${className}`} style={{ margin: '0.5vmin' }}>
-      <Alert variant="filled" severity="error">
-        Произошла ошибка: {message}
-      </Alert>
+    <div className="error">
+      <div className="error__wrap">
+        <div className="error__mes">{`${text}: ${error.message}`}</div>
+        <button onClick={handleClick} className="error__btn">Повторить запрос</button>
+      </div>
     </div>
-  );
+  )
 }
