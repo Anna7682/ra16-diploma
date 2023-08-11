ae3b67f815689b4557e57a7a29d8cd23fa2bd9f0
Anna7682 <107514746+Anna7682@users.noreply.github.com>
2023-08-11 15:46:59 +0300

    Update CardList.tsx

--- src/components/Main/Catalog/CardList/CardList.tsx
+++ src/components/Main/Catalog/CardList/CardList.tsx
@@ -1,9 +1,9 @@
 import React, { ReactNode } from 'react'
-import { useAppSelector } from '../../../../hooks/hooks'
-import { fetchCatalogItems } from '../../../../slices/asyncThunkCreator';
-import { selectCatalogError, selectCatalogSearch } from '../../../../slices/catalogSlice/catalogSlice';
-import { selectCategoriesSelected } from '../../../../slices/categorySlice/categorySlice';
-import { Error } from '../../../Error/Error'
+import { useAppSelector } from '../hooks/hooks'
+import { fetchCatalogItems } from '../slices/asyncThunkCreator';
+import { selectCatalogError, selectCatalogSearch } from '../slices/catalogSlice/catalogSlice';
+import { selectCategoriesSelected } from '../slices/categorySlice/categorySlice';
+import { Error } from '../../Error/Error'
 
 type TProps = { children: ReactNode }
 
