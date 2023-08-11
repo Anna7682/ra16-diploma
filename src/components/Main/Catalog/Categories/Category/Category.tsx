88fc3a7f6dedc529cd33dfb0154e37c3e7a3678c
Anna7682 <107514746+Anna7682@users.noreply.github.com>
2023-08-11 15:48:03 +0300

    Update Category.tsx

--- src/components/Main/Catalog/Categories/Category/Category.tsx
+++ src/components/Main/Catalog/Categories/Category/Category.tsx
@@ -1,9 +1,9 @@
-import { useAppDispatch, useAppSelector } from '../../../../../hooks/hooks';
-import { selectCategoriesSelected, setSelected } from '../../../../../slices/categorySlice/categorySlice';
-import { ICategory } from '../../../../../slices/catalogSlice/interfaces';
-import { fetchCatalogItems } from '../../../../../slices/asyncThunkCreator';
-import { } from '../../../../../slices/categorySlice/categorySlice'
-import { selectCatalogSearch } from '../../../../../slices/catalogSlice/catalogSlice';
+import { useAppDispatch, useAppSelector } from '../hooks/hooks';
+import { selectCategoriesSelected, setSelected } from '../slices/categorySlice/categorySlice';
+import { ICategory } from '../slices/catalogSlice/interfaces';
+import { fetchCatalogItems } from '../slices/asyncThunkCreator';
+import { } from '../slices/categorySlice/categorySlice'
+import { selectCatalogSearch } from '../slices/catalogSlice/catalogSlice';
 
 type TItem = { category: ICategory }
 
