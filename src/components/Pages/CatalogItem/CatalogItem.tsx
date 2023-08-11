45a5fa2a5826354e84b9dddfe05d94fbde9db09f
Anna7682 <107514746+Anna7682@users.noreply.github.com>
2023-08-11 16:37:54 +0300

    Update CatalogItem.tsx

--- src/components/Pages/CatalogItem/CatalogItem.tsx
+++ src/components/Pages/CatalogItem/CatalogItem.tsx
@@ -1,15 +1,15 @@
 import { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import error_img from '../../../img/no_image.png';
+import error_img from '../../../../img/no_image.png';
 import { nanoid } from 'nanoid';
-import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
-import { fetchProduct } from '../../slices/asyncThunkCreator';
-import { increment, decrement, setTheSize, resetSize } from '../../slices/productPageSlice/productPageSlice';
+import { useAppDispatch, useAppSelector } from '../../../hooks/hooks';
+import { fetchProduct } from '../../../slices/asyncThunkCreator';
+import { increment, decrement, setTheSize, resetSize } from '../../../slices/productPageSlice/productPageSlice';
 import { Error } from '../../Error/Error';
 import { Preloader } from '../../Main/Preloader/Preloader';
 import { Paths } from '../../../Paths';
-import { ICartItem } from '../../slices/cartSlice/interfaces';
-import { addProductToCart, updateCart } from '../../slices/cartSlice/cartSlice';
+import { ICartItem } from '../../../slices/cartSlice/interfaces';
+import { addProductToCart, updateCart } from '../../../slices/cartSlice/cartSlice';
 import {
   selectAvalible,
   selectProduct,
